dd3a006fad8fdca782396da35b37bb94764c3bcd
lluuvvii <kurniawanluvi64@gmail.com>
2024-07-06 22:22:21 +0700

    update layout text

--- src/app/components/introduction/IntroductionCard.tsx
+++ src/app/components/introduction/IntroductionCard.tsx
@@ -14,37 +14,38 @@ const IntroductionCard = () => {
       <Box sx={{ borderRadius: '10px', backgroundColor: 'white', padding: '10px', boxShadow: 'inset 0px 0px 5px rgba(0, 0, 0, 0.5)' }}>
         <Grid container spacing={1}>
           <Grid item xs={12}>
-            <Stack direction='column' alignItems='center' mb={2}>
-              <Typography
-                variant='h5'
-                sx={{
-                  fontFamily: 'Nunito, Arial, sans-serif',
-                  fontWeight: 700,
-                  color: 'white',
-                  textShadow: '0px 2.5px 1px rgba(0,0,0,1), 0px -1px 1px rgba(0,0,0,1), 2px -0px 1px rgba(0,0,0,1), -2px 0px 1px rgba(0,0,0,1), 2px 2px 1px rgba(0,0,0,1), -2px 2px 1px rgba(0,0,0,1)'
-                }}
-              >
-                Hi, My name is
-              </Typography>
-              <Typography
-                variant='h5'
-                sx={{
-                  fontFamily: 'Nunito, Arial, sans-serif',
-                  fontWeight: 700,
-                  color: 'yellow',
-                  textShadow: '0px 2.5px 1px rgba(0,0,200,1), 0px -1px 1px rgba(0,0,200,1), 2px -0px 1px rgba(0,0,200,1), -2px 0px 1px rgba(0,0,200,1), 2px 2px 1px rgba(0,0,200,1), -2px 2px 1px rgba(0,0,200,1)'
-                }}
-              >
-                I LOVE SHOLLAKHUDDIN KURNIAWAN
-              </Typography>
-            </Stack>
+            <Typography
+              variant='h5'
+              sx={{
+                textAlign: 'center',
+                fontFamily: 'Nunito, Arial, sans-serif',
+                fontWeight: 700,
+                color: 'white',
+                textShadow: '0px 2.5px 1px rgba(0,0,0,1), 0px -1px 1px rgba(0,0,0,1), 2px -0px 1px rgba(0,0,0,1), -2px 0px 1px rgba(0,0,0,1), 2px 2px 1px rgba(0,0,0,1), -2px 2px 1px rgba(0,0,0,1)'
+              }}
+            >
+              Hi, My name is
+            </Typography>
+            <Typography
+              variant='h5'
+              ml={1}
+              mb={2}
+              sx={{
+                textAlign: 'center',
+                fontFamily: 'Nunito, Arial, sans-serif',
+                fontWeight: 700,
+                color: 'yellow',
+                textShadow: '0px 2.5px 1px rgba(0,0,200,1), 0px -1px 1px rgba(0,0,200,1), 2px -0px 1px rgba(0,0,200,1), -2px 0px 1px rgba(0,0,200,1), 2px 2px 1px rgba(0,0,200,1), -2px 2px 1px rgba(0,0,200,1)'
+              }}
+            >
+              I LOVE SHOLLAKHUDDIN KURNIAWAN
+            </Typography>
             <Typography
               variant='body1'
               sx={{
                 fontFamily: 'Nunito, Arial, sans-serif',
                 fontWeight: 700,
                 color: 'black',
-                // textShadow: '0px 2.5px 1px rgba(0,0,0,1), 0px -1px 1px rgba(0,0,0,1), 2px -0px 1px rgba(0,0,0,1), -2px 0px 1px rgba(0,0,0,1), 2px 2px 1px rgba(0,0,0,1), -2px 2px 1px rgba(0,0,0,1)'
               }}
             >
               I'm a fullstack web developer passionate about getting to the software engineer level, I'm currently pursuing a degree in computer engineering and dedicating my time to gaining knowledge that I will apply in the future.
