6ff37596994b22a28d3bb8c4e4b181d9c1f3f5b8
lluuvvii <kurniawanluvi64@gmail.com>
2024-07-08 16:02:09 +0700

    add more particles right side banner

--- src/app/components/introduction/IntroBanner.tsx
+++ src/app/components/introduction/IntroBanner.tsx
@@ -6,6 +6,238 @@ const IntroBanner = () => {
   return (
     <Box sx={{ position: 'absolute', width: '100%', transform: 'rotate(-2deg)', mt: 10 }}>
       <ZigzagContainer width='100%' height='300px' color='yellow' toColor='#ffc800' toGradient='to right' top bottom />
+      {/* bottom left side particles */}
+      {/* striped circle */}
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '50px',
+          height: '50px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          background: 'repeating-linear-gradient(45deg, black, black, 6px, transparent 6px, transparent 11px)',
+          filter: 'drop-shadow(-2px 2px 2px rgba(0,0,0,0.5))',
+          bottom: '100px',
+          left: '-20px',
+        }}>
+      </Box>
+      {/* circle */}
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '120px',
+          height: '120px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+          bottom: '-10px',
+          left: '-50px',
+        }}
+      >
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '60px',
+          height: '60px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'red',
+          bottom: '70px',
+          left: '-30px',
+        }}
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            width: '40px',
+            height: '40px',
+            borderRadius: '50%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'yellow',
+          }}
+        >
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '100px',
+          height: '100px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'transparent',
+          border: '20px solid red',
+          bottom: '-10px',
+          left: '-50px',
+        }}
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            width: '90px',
+            height: '90px',
+            borderRadius: '50%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'transparent',
+            border: '10px solid white',
+          }}
+        ></Box>
+      </Box>
+      {/* striped circle */}
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '80px',
+          height: '80px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          background: 'repeating-linear-gradient(45deg, yellow, yellow, 8px, transparent 8px, transparent 15px)',
+          filter: 'drop-shadow(-2px 2px 2px rgba(0,0,0,0.5))',
+          bottom: '-15px',
+          left: '0px',
+        }}>
+      </Box>
+      {/* top right side particles */}
+      {/* star long shadow */}
+      <Box
+        sx={{
+          position: 'absolute',
+          '& svg': {
+            filter: 'drop-shadow(-2px 3px 10px rgba(0,0,0,0.5))',
+          },
+          top: '-15px',
+          right: '80px'
+        }}
+      >
+        <div style={{ transform: 'scale(0.12) rotate(315deg)', width: '1px', height: '1px' }}>
+          <svg
+            width='200px'
+            height='1000px'
+            fill={'red'}
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <rect x='0' y='80' width='100%' height='100%' fill={'red'} />
+            <path d='M100 0.587l30.52 66.76L200 81.25l-50 48.5L161.92 200 100 165.83 38.08 200 50 129.75 0 81.25l69.44-13.29z' />
+            <path d='M100 0.587l30.52 66.76L200 81.25l-50 48.5L161.92 200 100 165.83 38.08 200 50 129.75 0 81.25l69.44-13.29z' transform='scale(0.7) translate(42.5, 47)' fill='white' />
+          </svg>
+        </div>
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          '& svg': {
+            filter: 'drop-shadow(-2px 3px 10px rgba(0,0,0,0.5))',
+          },
+          top: '45px',
+          right: '90px'
+        }}
+      >
+        <div style={{ transform: 'scale(0.1) rotate(315deg)', width: '1px', height: '1px' }}>
+          <svg
+            width='200px'
+            height='1000px'
+            fill={'black'}
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <rect x='0' y='80' width='100%' height='100%' fill={'black'} />
+            <path d='M100 0.587l30.52 66.76L200 81.25l-50 48.5L161.92 200 100 165.83 38.08 200 50 129.75 0 81.25l69.44-13.29z' />
+            <path d='M100 0.587l30.52 66.76L200 81.25l-50 48.5L161.92 200 100 165.83 38.08 200 50 129.75 0 81.25l69.44-13.29z' transform='scale(0.7) translate(42.5, 47)' fill='white' />
+          </svg>
+        </div>
+      </Box>
+      {/* circle */}
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '70px',
+          height: '70px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+          top: '50px',
+          right: '-20px',
+        }}
+      >
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '40px',
+          height: '40px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+          top: '40px',
+          right: '20px',
+        }}
+      >
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '100px',
+          height: '100px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'yellow',
+          border: '10px solid red',
+          top: '-10px',
+          right: '-50px',
+        }}
+      >
+      </Box>
+      {/* striped circle */}
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '80px',
+          height: '80px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          background: 'repeating-linear-gradient(45deg, yellow, yellow, 8px, transparent 8px, transparent 15px)',
+          filter: 'drop-shadow(-2px 2px 2px rgba(0,0,0,0.5))',
+          top: '40px',
+          right: '-40px',
+        }}>
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '40px',
+          height: '40px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          background: 'repeating-linear-gradient(45deg, red, red, 5px, transparent 5px, transparent 9px)',
+          filter: 'drop-shadow(-2px 2px 2px rgba(0,0,0,0.5))',
+          top: '-40px',
+          right: '-30px',
+        }}>
+      </Box>
       <Typography
         variant='h4'
         sx={{
