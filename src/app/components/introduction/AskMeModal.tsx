f1d82d6a938e4f04a023d04d91abc197aa12c20f
lluuvvii <kurniawanluvi64@gmail.com>
2024-07-17 11:00:21 +0700

    update particles ask me modal

--- src/app/components/introduction/AskMeModal.tsx
+++ src/app/components/introduction/AskMeModal.tsx
@@ -7,6 +7,100 @@ import { motion } from 'framer-motion'
 const AskMeModal = () => {
   return (
     <Box sx={{ position: 'relative' }}>
+      {/* circle */}
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '30px',
+          height: '30px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+          top: '0px',
+          left: '5px',
+        }}
+      >
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '40px',
+          height: '40px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+          top: '0px',
+          left: '25px',
+        }}
+      >
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '30px',
+          height: '30px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'red',
+          top: '5px',
+          left: '50px',
+        }}
+      >
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '20px',
+          height: '20px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'transparent',
+          border: '3px solid red',
+          top: '15px',
+          left: '75px',
+        }}
+      >
+      </Box>
+      {/* striped circle */}
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '40px',
+          height: '40px',
+          borderRadius: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          background: 'repeating-linear-gradient(45deg, yellow, yellow, 4px, transparent 4px, transparent 7px)',
+          filter: 'drop-shadow(-2px 2px 2px rgba(0,0,0,0.5))',
+          top: '5px',
+          left: '-10px',
+        }}>
+      </Box>
+      {/* spike */}
+      <Box
+        sx={{
+          position: 'absolute',
+          width: '40px',
+          height: '40px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'red',
+          clipPath: 'polygon(100% 50%,83.81% 59.06%,93.3% 75%,74.75% 74.75%,75% 93.3%,59.06% 83.81%,50% 100%,40.94% 83.81%,25% 93.3%,25.25% 74.75%,6.7% 75%,16.19% 59.06%,0% 50%,16.19% 40.94%,6.7% 25%,25.25% 25.25%,25% 6.7%,40.94% 16.19%,50% 0%,59.06% 16.19%,75% 6.7%,74.75% 25.25%,93.3% 25%,83.81% 40.94%)',
+          top: '10px',
+          left: '0px'
+        }}
+      >
+      </Box>
       <Box
         component={motion.div}
         whileHover={{ scale: 1.1 }}
