f2c69c9373ca49092b70869d4ab793d8767d9656
lluuvvii <kurniawanluvi64@gmail.com>
2024-09-03 21:14:53 +0700

    update background color temporary

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -13,7 +13,8 @@ const Home = () => {
           height: '100vh',
           top: 0,
           left: 0,
-          background: 'linear-gradient(-15deg, black 6%, #d10000 6%, #d10000 8%, black 8%, black 17%, #d10000 17%, #d10000 42%, white 42%, white 50%, #d10000 50%, #d10000 85%, black 85%, black 87%, #d10000 87%, #d10000 90%, black 90%)',
+          // background: 'linear-gradient(-15deg, black 6%, #d10000 6%, #d10000 8%, black 8%, black 17%, #d10000 17%, #d10000 42%, white 42%, white 50%, #d10000 50%, #d10000 85%, black 85%, black 87%, #d10000 87%, #d10000 90%, black 90%)',
+          backgroundColor: 'rgba(20, 20, 20, 1)'
         }} />
         <Grid container spacing={2} rowSpacing={3} justifyContent='center'>
           <Grid item>
