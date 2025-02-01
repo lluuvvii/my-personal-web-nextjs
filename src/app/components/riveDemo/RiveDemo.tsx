2d259d407fc8a99516381c49f935a120da0fae5f
lluuvvii <kurniawanluvi64@gmail.com>
2025-02-01 23:27:48 +0700

    add another riv with destructure object

--- src/app/components/riveDemo/RiveDemo.tsx
+++ src/app/components/riveDemo/RiveDemo.tsx
@@ -3,7 +3,7 @@
 import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas'
 
 const RiveDemo = () => {
-  const { RiveComponent } = useRive({
+  const { RiveComponent: DumbWays } = useRive({
     // Load a local riv `clean_the_car.riv` or upload your own!
     src: 'eletrical_wire_-_dumb_ways_to_die.riv',
     // Be sure to specify the correct state machine (or animation) name
@@ -16,9 +16,24 @@ const RiveDemo = () => {
     autoplay: true,
   })
 
+  const { RiveComponent: IsoToy } = useRive({
+    src: 'iso_toy_001.riv',
+    stateMachines: 'State Machine 1',
+    layout: new Layout({
+      fit: Fit.FitWidth,
+      alignment: Alignment.Center,
+    }),
+    autoplay: true,
+  })
+
   return (
-    <div style={{ width: '100%', height: '800px' }}>
-      <RiveComponent />
+    <div>
+      <div style={{ width: '100%', height: '800px' }}>
+        <DumbWays />
+      </div>
+      <div style={{ width: '100%', height: '800px' }}>
+        <IsoToy />
+      </div>
     </div>
   )
 }
