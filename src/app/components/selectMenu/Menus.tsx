26fa35fc777dad014b4b74701e90acf63b7b5810
lluuvvii <kurniawanluvi64@gmail.com>
2025-02-16 23:13:38 +0700

    update shadow layer text

--- src/app/components/selectMenu/Menus.tsx
+++ src/app/components/selectMenu/Menus.tsx
@@ -35,7 +35,7 @@ export default function SelectMenu() {
       tabIndex={0}
       onKeyDown={handleKeyDown}
       sx={{ overflow: 'hidden', height: '100vh' }}>
-      <Box sx={{ position: 'relative', top: '30vh' }}>
+      <Box sx={{ position: 'relative', top: '30vh', }}>
         {menuItems.map((item, index) => {
           const angle = (index - selectedIndex) * 8
 
@@ -56,18 +56,42 @@ export default function SelectMenu() {
                 marginTop: { xs: '-0.5rem', sm: '-0.7rem', md: '-0.9rem', lg: '-1.1rem' },
               }}>
               {selectedIndex === index ?
-                <Box
-                  component={motion.div}
-                  initial={{ width: '0%', x: '-5%' }}
-                  animate={{ width: '110%' }}
-                  transition={{ duration: 0.2, ease: 'easeInOut' }}
-                  sx={{
-                    position: 'absolute',
-                    inset: 0,
-                    height: '100%',
-                    backgroundColor: '#ef4444',
-                  }}
-                />
+                <>
+                  <Typography
+                    component={motion.div}
+                    initial={{ x: '0%', scaleX: 2, scaleY: 1.5 }}
+                    animate={{ x: '100%' }}
+                    transition={{ duration: 0.2, ease: [0, 0.71, 0.2, 1.01] }}
+                    variant='h4'
+                    sx={{
+                      position: 'absolute',
+                      bottom: '-40%',
+                      fontSize: { xs: '1.5rem', sm: '2rem', md: '2.5rem', lg: '3rem' },
+                      fontVariant: 'small-caps',
+                      fontWeight: 'bold',
+                      textAlign: 'start',
+                      color: '#ef4444',
+                      opacity: 0.5,
+                      transition: 'color 0.1s ease-in-out',
+                    }}
+                  >
+                    {item}
+                  </Typography>
+                  <Box
+                    component={motion.div}
+                    initial={{ width: '0%', x: '-5%' }}
+                    animate={{ width: '110%' }}
+                    transition={{ duration: 0.2, ease: 'easeInOut' }}
+                    sx={{
+                      position: 'absolute',
+                      inset: 0,
+                      height: '100%',
+                      backgroundColor: '#ef4444',
+                      transformStyle: 'preserve-3d',
+                      transform: 'rotateY(-10deg)'
+                    }}
+                  />
+                </>
                 : null}
               <Typography
                 variant='h4'
