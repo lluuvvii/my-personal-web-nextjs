685a5047939bf6d26e5261fdcb2ba16d9bf597b5
lluuvvii <kurniawanluvi64@gmail.com>
2025-06-11 14:43:03 +0700

    unuse selectmenu in /test/page.tsx to fix build error

--- src/app/(Home)/test/page.tsx
+++ src/app/(Home)/test/page.tsx
@@ -1,6 +1,5 @@
 import ZigzagContainer from '@/app/components/materials/ZigzagContainer'
 import RiveDemo from '@/app/components/riveDemo/RiveDemo'
-import SelectMenu from '@/app/components/selectMenu/Menus'
 import GithubStats from '@/app/components/stats/GithubStats'
 import { Box, Grid, Stack, Typography } from '@mui/material'
 import Image from 'next/image'
@@ -94,7 +93,6 @@ const BackgroundContainer = () => {
           </Grid>
         </ZigzagContainer>
       </Box>
-      <SelectMenu />
     </>
   )
 }
